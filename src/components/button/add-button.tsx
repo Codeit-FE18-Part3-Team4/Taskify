04edb3bee8f02316b4360c82758c553359685f6b
jeyoung <jdngh@naver.com>
2025-09-30 13:19:06 +0900

    ✨ feat [#4] 대시보드 추가, 삭제 컴포넌트 추가

--- src/components/button/add-button.tsx
+++ src/components/button/add-button.tsx
@@ -3,25 +3,38 @@ import styles from "./add-button.module.css";
 
 type AddButtonProps = {
   children?: string;
-  icon?: string;
+  rightIcon?: string;
+  variant?: "nomal" | "dashboard" | "delete";
   onClick: React.MouseEventHandler<HTMLButtonElement>;
 };
 
-// 컬럼 추가, 투두 추가 버튼
-export default function AddButton({ children, icon, onClick }: AddButtonProps) {
+/**
+ * 컬럼, 투두, 대시보드 추가 / 삭제 버튼
+ */
+export default function AddButton({
+  children,
+  rightIcon,
+  variant = "nomal",
+  onClick,
+}: AddButtonProps) {
   const columnButton = children ? "" : styles.TodoButton;
 
   return (
-    <button className={`${styles.button} ${columnButton}`} onClick={onClick}>
+    <button
+      className={`${styles.button} ${columnButton} ${styles[variant]}`}
+      onClick={onClick}
+    >
       {children && <span className={styles.span}>{children}</span>}
-      {icon && (
-        <Image
-          src={icon}
-          width={20}
-          height={20}
-          alt="button icon"
-          className={styles.icon}
-        />
+      {rightIcon && (
+        <div className={ styles.iconBtn }>
+          <Image
+            src={rightIcon}
+            width={20}
+            height={20}
+            alt="button icon"
+            className={styles.icon}
+          />
+        </div>
       )}
     </button>
   );
