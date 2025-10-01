e626353ebb2245d207e74c43a5f519781339965f
jeyoung <jdngh@naver.com>
2025-10-01 21:43:31 +0900

    ✨ feat [#4] 버튼 컴포넌트

---
+++ src/components/button/button.tsx
@@ -0,0 +1,43 @@
+import { ReactNode } from "react";
+import styles from "./button.module.css";
+
+export enum ButtonVariant {
+  Primary = "primary",
+  Secondary = "secondary",
+  Delete = "delete",
+}
+
+export enum ButtonSize {
+  Large = "lg",
+  Medium = "md",
+  Small = "sm",
+  XSmall = "xs",
+  Auto = "auto",
+}
+
+interface ButtonProps {
+  children: ReactNode;
+  variant?: ButtonVariant;
+  size?: ButtonSize;
+  disabled?: boolean;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export default function Button({
+  children,
+  variant = ButtonVariant.Primary,
+  size = ButtonSize.Auto,
+  disabled,
+  onClick,
+}: ButtonProps) {
+  const DisabledStyle = disabled === true ? styles.disabled : "";
+  return (
+    <button
+      className={`${styles.button} ${styles[variant]} ${styles[size]} ${DisabledStyle}`}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      <span>{children}</span>
+    </button>
+  );
+}
