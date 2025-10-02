2c4c1a78cf45968d807bce8e76386e29e9a78010
Chamsol Kim <kcsol1005@gmail.com>
2025-10-02 13:15:50 +0900

    ✨ feat [#14] Modal에서 content 밖을 클릭해서 닫기 구현

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,6 +1,6 @@
 import { useModal } from "@/hooks/use-modal";
 import { classnames } from "@/utils/classnames";
-import { ReactNode } from "react";
+import { MouseEvent, ReactNode } from "react";
 import { createPortal } from "react-dom";
 import styles from "./modal.module.css";
 
@@ -10,19 +10,30 @@ interface Props {
 }
 
 export default function Modal({ isOpen, children }: Props) {
-  const { onClose } = useModal();
+  const { openModal, onClose } = useModal();
 
   const handleAnimationEnd = () => {
     if (isOpen) return;
     onClose();
   };
 
+  const handleClick = () => {
+    openModal(false);
+  };
+
+  const handleContentClick = (event: MouseEvent) => {
+    event.stopPropagation();
+  };
+
   const modal = (
     <div
       className={classnames(styles.modal, isOpen ? styles.open : styles.close)}
       onAnimationEnd={handleAnimationEnd}
+      onClick={handleClick}
     >
-      <div className={styles.content}>{children}</div>
+      <div className={styles.content} onClick={handleContentClick}>
+        {children}
+      </div>
     </div>
   );
 
