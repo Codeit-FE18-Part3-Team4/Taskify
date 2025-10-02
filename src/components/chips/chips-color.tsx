14bc3e87d2a74a660e48074626141e19ed376dc1
Luli Lee🦆 <nidor022@gmail.com>
2025-10-03 03:34:19 +0900

    Revert "✨ feat [#11] 컬러팔레트 개별컴포넌트 개발"
    This reverts commit a8aef861a8b6f2ef93f6c6b3a79a2ed457e10506.

--- src/components/chips/chips-color.tsx
+++ src/components/chips/chips-color.tsx
@@ -10,9 +10,9 @@ interface ChipColorProps {
 
 export default function ColorChip({
   color = CHIP_COLORS[0],
-  size = CommonSize.Small,
+  size = CommonSize.small,
 }: ChipColorProps) {
-  const sizeName = CommonSize[size].toLowerCase();
+  const sizeName = CommonSize[size];
   const chipClasses = `${styles[`color_chip_${sizeName}`]} color-chip`;
 
   return <ColorChipSvg className={chipClasses} color={color} />;
