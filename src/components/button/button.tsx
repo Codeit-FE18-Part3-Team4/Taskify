cae20f01612d250a86d6fcedb81e4d20f05cbe13
jeyoung <jdngh@naver.com>
2025-10-02 19:09:08 +0900

    ♻️ refactor [#4] 폰트 props로 받기 추가, 넓이 높이 full 사이즈 추가, 컬러 변수로 변경

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,4 +1,5 @@
 import { MouseEventHandler, ReactNode } from "react";
+import Typography from "../typography/typography";
 import styles from "./button.module.css";
 
 export enum ButtonVariant {
@@ -15,11 +16,20 @@ export enum ButtonSize {
   Auto = "auto",
 }
 
+export const ButtonFontSize = {
+  lg2SemiBold: Typography.lg2SemiBold,
+  lgSemiBold: Typography.lgSemiBold,
+  mdSemiBold: Typography.mdSemiBold
+} as const;
+
 interface ButtonProps {
   children: ReactNode;
   variant?: ButtonVariant;
   size?: ButtonSize;
+  fontSize?: typeof ButtonFontSize[keyof typeof ButtonFontSize];
   disabled?: boolean;
+  isWidthFull?: boolean;
+  isHeightFull?: boolean;
   onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
@@ -27,13 +37,16 @@ export default function Button({
   children,
   variant = ButtonVariant.Primary,
   size = ButtonSize.Auto,
+  fontSize = ButtonFontSize.lg2SemiBold,
   disabled,
+  isWidthFull,
+  isHeightFull,
   onClick,
 }: ButtonProps) {
   const disabledStyle = disabled === true ? styles.disabled : "";
   return (
     <button
-      className={`${styles.button} ${styles[variant]} ${styles[size]} ${disabledStyle}`}
+      className={`${styles.button} ${styles[variant]} ${styles[size]} ${fontSize} ${isWidthFull ? styles.widthFull : ""} ${isHeightFull ? styles.heightFull : ""} ${disabledStyle}`}
       disabled={disabled}
       onClick={onClick}
     >
