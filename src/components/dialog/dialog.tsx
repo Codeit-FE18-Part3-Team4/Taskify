e840801fe60d417eac66a87c1336c8bb9907ca84
Chamsol Kim <kcsol1005@gmail.com>
2025-10-03 20:30:21 +0900

    ✨ feat [#27] 3가지 반응형 크기를 감지하는 `useResponsive` custom hook 사용

--- src/components/dialog/dialog.tsx
+++ src/components/dialog/dialog.tsx
@@ -1,7 +1,7 @@
 import Typography from "@/components/typography/typography";
+import { useResponsive } from "@/hooks/ues-responsive";
 import { useDialog } from "@/hooks/use-dialog";
 import { classnames } from "@/utils/classnames";
-import { useMediaQuery } from "@uidotdev/usehooks";
 import { useMemo } from "react";
 import Button, { ButtonSize, ButtonVariant } from "../button/button";
 import Modal from "../modal/modal";
@@ -15,7 +15,7 @@ interface Props {
 
 export default function Dialog({ dialogKey, message, onConfirm }: Props) {
   const { openDialog } = useDialog({ key: dialogKey });
-  const isMobile = useMediaQuery("(max-width: 375px)");
+  const { isMobile } = useResponsive();
 
   const messageTypography = useMemo(() => {
     return isMobile ? Typography.lgSemiBold : Typography.xlSemiBold;
