70af5c76b146bc69cd6bb40d530c447802230000
JinHyuk <dog33027@gmail.com>
2025-10-04 21:28:26 +0900

    ✨ feat [#57] svg 아이콘의 viewBox를 size에 맞게 수정

--- src/components/input/visibility-off-svg.tsx
+++ src/components/input/visibility-off-svg.tsx
@@ -11,7 +11,7 @@ export default function VisibilityNowOffIcon({
     <svg
       width={size}
       height={size}
-      viewBox="0 0 24 24"
+      viewBox={`0 0 ${size} ${size}`}
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
