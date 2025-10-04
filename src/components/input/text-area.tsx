3f3ae58aadf40c74eb2ecd1c9c1e2c5a2f91a169
JinHyuk <dog33027@gmail.com>
2025-10-04 13:41:22 +0900

    ✨ feat [#43] TextArea props를 TextareaHTMLAttributes로 변경

--- src/components/input/text-area.tsx
+++ src/components/input/text-area.tsx
@@ -1,31 +1,15 @@
 import styles from "./text-area.module.css";
-import { ChangeEvent, FocusEvent } from "react";
+import { TextareaHTMLAttributes } from "react";
 import Typography from "@/components/typography/typography";
 
-interface TextAreaProps {
-  value?: string;
-  onChange?: (e: ChangeEvent<HTMLTextAreaElement>) => void;
-  onBlur?: (e: FocusEvent<HTMLTextAreaElement>) => void;
-  placeholder?: string;
-  disabled?: boolean;
+interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   className?: string;
 }
 
-export default function TextArea({
-  value,
-  onChange,
-  onBlur,
-  placeholder,
-  disabled,
-  className,
-}: TextAreaProps) {
+export default function TextArea({ className, ...props }: TextAreaProps) {
   return (
     <textarea
-      disabled={disabled}
-      value={value}
-      onChange={onChange}
-      onBlur={onBlur}
-      placeholder={placeholder}
+      {...props}
       className={`${styles.textarea} 
                   ${Typography.lgMedium} 
                   ${className ?? ""} `}
