601342418ebe13625524466f8d884704960acd3d
jeyoung <jdngh@naver.com>
2025-10-04 10:26:47 +0900

    ✨ feat [#54] 메인 랜딩페이지 header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,23 @@
-import type { NextPage } from 'next';
+import Logo from "@/assets/images/taskify-logo.svg";
+import Typography from "@/components/typography/typography";
+import styles from "@/styles/main.module.css";
+import type { NextPage } from "next";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
-    <main>
-      <h1>🚀 Part3 4팀 화이팅 🚀</h1>
-    </main>
+    <div className={styles.mainWrap}>
+      <header className={styles.header}>
+        <div className={styles.logo}>
+          <Image src={Logo} width={186} height={48} alt="로고 이미지" />
+        </div>
+        <div className={ styles.headerButtonBox}>
+          <button className={Typography.lgMedium}>로그인</button>
+          <button className={Typography.lgMedium}>회원가입</button>
+        </div>
+      </header>
+      
+    </div>
   );
 };
 
