0aa1b418788e0355bb93480cb4a6885461fbae2c
Chamsol Kim <kcsol1005@gmail.com>
2025-10-05 23:26:47 +0900

    ✨ feat [#59] Dropdown option이 scrollable 될 때 layout을 다르게 설정
    Option에 hover 효과가 적용될 때 좌우 여백을 제거하기 위해 동적으로 layout 변경

--- src/components/dropdown/index.tsx
+++ src/components/dropdown/index.tsx
@@ -1,7 +1,47 @@
+import Typography from "@/components/typography";
+import { useResponsive } from "@/hooks/use-responsive";
 import { classnames } from "@/utils/classnames";
-import { ReactNode, useState } from "react";
-import Typography from "../typography";
+import { ReactNode, useMemo, useState } from "react";
 import styles from "./dropdown.module.css";
+
+function DropdownContainer({
+  options,
+  onSelect,
+}: {
+  options: ReactNode[];
+  onSelect?: (index: number) => void;
+}) {
+  const { isMobile } = useResponsive();
+
+  const scrollableOrEmpty = useMemo(() => {
+    const maxVisibleOptions = isMobile ? 3 : 5;
+    return options.length > maxVisibleOptions ? styles.scrollable : "";
+  }, [options, isMobile]);
+
+  const handleOptionClick = (index: number) => {
+    onSelect?.(index);
+  };
+
+  return (
+    <div className={classnames(styles.dropdown, scrollableOrEmpty)}>
+      <div className={styles.dropdownContent}>
+        {options.map((option, index) => (
+          <div
+            key={`dropdown-${index}`}
+            className={classnames(
+              styles.dropdownOption,
+              scrollableOrEmpty,
+              Typography.lgMedium
+            )}
+            onClick={() => handleOptionClick(index)}
+          >
+            {option}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 interface Props {
   options: ReactNode[];
@@ -16,7 +56,7 @@ export default function Dropdown({ options, children, onSelect }: Props) {
     setShow((prev) => !prev);
   };
 
-  const handleOptionClick = (index: number) => {
+  const handleOptionSelect = (index: number) => {
     onSelect?.(index);
     setShow(false);
   };
@@ -25,22 +65,7 @@ export default function Dropdown({ options, children, onSelect }: Props) {
     <div className={styles.container}>
       <div onClick={handleAnchorClick}>{children}</div>
       {isShow && (
-        <div className={styles.dropdown}>
-          <div className={styles.dropdownContent}>
-            {options.map((option, index) => (
-              <div
-                key={`dropdown-${index}`}
-                className={classnames(
-                  styles.dropdownOption,
-                  Typography.lgMedium
-                )}
-                onClick={() => handleOptionClick(index)}
-              >
-                {option}
-              </div>
-            ))}
-          </div>
-        </div>
+        <DropdownContainer options={options} onSelect={handleOptionSelect} />
       )}
     </div>
   );
