92f62e2d9ccbb368a36119e0b574b257abc85dd8
Luli Lee🦆 <nidor022@gmail.com>
2025-10-11 23:43:52 +0900

    ✨ feat [#82] badge, navigation bar 누락코드 및 오류 수정

--- src/components/chips/badge/badge.tsx
+++ src/components/chips/badge/badge.tsx
@@ -1,6 +1,7 @@
 import styles from "./badge.module.css";
 import { BADGE_COLORS } from "@/components/chips/badge/badge-colors";
 import typographyStyles from "@/components/typography/typography.module.css";
+import { useMemo } from "react";
 
 interface BadgeProps {
   colorIndex?: number;
@@ -9,7 +10,15 @@ interface BadgeProps {
 
 export default function Badge({ colorIndex = 0, title = "" }: BadgeProps) {
   const badgeClasses = `${styles.badge} ${typographyStyles["sm-semibold"]}`;
-  const calculateColorIndex = colorIndex % BADGE_COLORS.length;
+  const calculateColorIndex = useMemo(() => {
+    if (colorIndex !== undefined) {
+      return colorIndex % BADGE_COLORS.length;
+    }
+    const hash = title.split("").reduce((acc, char, index) => {
+      return acc + char.charCodeAt(0) * (index + 1);
+    }, 0);
+    return hash % BADGE_COLORS.length;
+  }, [colorIndex, title]);
 
   return (
     <span className={badgeClasses} style={BADGE_COLORS[calculateColorIndex]}>
