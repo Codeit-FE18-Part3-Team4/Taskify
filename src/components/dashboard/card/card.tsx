a8e5d02d0966fba54c29873e617c90de94e45cb3
luli <nidor022@gmail.com>
2025-10-11 23:24:12 +0900

    ✨ feat [#50] 브랜치이동해야해서 임시커밋

--- src/components/dashboard/card/card.tsx
+++ src/components/dashboard/card/card.tsx
@@ -1,9 +1,21 @@
+import Profile from "@/components/profile/profile";
 import styles from "./card.module.css";
 
-export default function ColumnCard() {
+interface CardProps {
+  title?: string;
+  tags?: string[];
+}
+
+export default function Card() {
   return (
-    <>
-      <h1>대쉬보드 컬럼카드</h1>
-    </>
+    <section className={styles.cardContainer}>
+      <span className={styles.title}>test</span>
+      <div className="badge">배지들어갈부분</div>
+      <span className="date">2025년 12월 25일</span>
+      <div className="writer">
+        <Profile />
+        <span className="userName">김아무개</span>
+      </div>
+    </section>
   );
 }
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,22 @@
+import { ReactElement } from "react";
 import styles from "./index.module.css";
+import NavigationBar from "@/components/navigationBar/navigation-bar";
+import DashboardSideBar from "@/components/dashboard-side-bar/dashboard-side-bar";
+import Column from "@/components/dashboard/column/column";
+import { MemberInfo } from "@/type/member-info";
 
-export default function DashBoard() {
+export default function DashboardPage() {
+  return <div className="DashboardContainer"></div>;
+}
+
+DashboardPage.getLayout = function getLayout(page: ReactElement) {
   return (
     <>
-      <h1>대쉬보드 페이지</h1>
+      <NavigationBar />
+      <div style={{ display: "flex" }}>
+        <DashboardSideBar />
+        <main style={{ flex: 1, padding: "20px" }}>{page}</main>
+      </div>
     </>
   );
-}
+};
