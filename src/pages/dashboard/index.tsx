9f23aa2339ac3490de6510fd9c38953b2efdf700
Luli Lee🦆 <nidor022@gmail.com>
2025-10-13 01:48:49 +0900

    ✨ feat [#50] layout 컴포넌트 css inline style에서 className스타일로 변경

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,19 +3,21 @@ import styles from "./index.module.css";
 import NavigationBar from "@/components/navigationBar/navigation-bar";
 import DashboardSideBar from "@/components/dashboard-side-bar/dashboard-side-bar";
 import Column from "@/components/dashboard/column/column";
-import { MemberInfo } from "@/type/member-info";
-
 export default function DashboardPage() {
-  return <div className="DashboardContainer"></div>;
+  return (
+    <div className="DashboardContainer">
+      <Column />
+    </div>
+  );
 }
 
 DashboardPage.getLayout = function getLayout(page: ReactElement) {
   return (
     <>
       <NavigationBar />
-      <div style={{ display: "flex" }}>
+      <div className={styles.layoutContainer}>
         <DashboardSideBar />
-        <main style={{ flex: 1, padding: "20px" }}>{page}</main>
+        <main className={styles.main}>{page}</main>
       </div>
     </>
   );
