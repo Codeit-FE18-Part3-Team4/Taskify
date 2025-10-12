d06d46ec4cb4f65427abaf10c7cf10b32226f942
Luli Lee🦆 <nidor022@gmail.com>
2025-10-12 19:26:04 +0900

    ✨ feat [#89] profile name 이름에따라 색상 변경하도록 코드 수정

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -7,6 +7,7 @@ import { ProfileSize } from "@/components/profile/profile-size";
 import { ProfileType } from "@/components/profile/profile-type";
 import typographyStyles from "@/components/typography/typography.module.css";
 import { classnames } from "@/utils/classnames";
+import { useMemo } from "react";
 
 interface ProfileProps {
   size?: ProfileSize;
@@ -19,24 +20,50 @@ interface ProfileProps {
 export default function Profile({
   size = ProfileSize.XSmall,
   type = ProfileType.Normal,
-  colorIndex = 0,
+  colorIndex,
   name = "",
   isRemain = false,
 }: ProfileProps) {
-  const calculateColorIndex = colorIndex % 7;
+  const calculateColorIndex = useMemo(() => {
+    if (colorIndex !== undefined) {
+      return colorIndex % ProfileColorsArray.length;
+    }
+
+    const hash = name.split("").reduce((acc, char, index) => {
+      return acc + char.charCodeAt(0) * (index + 1);
+    }, 0);
+
+    return hash % ProfileColorsArray.length;
+  }, [colorIndex, name]);
+
   const color = isRemain
     ? RemainProfileColor.backgroundColor
     : ProfileColorsArray[calculateColorIndex];
-  const NavigationBarClasses = classnames(
+
+  const getLength = (str: string) => {
+    return str.split("").reduce((acc, char) => {
+      const code = char.charCodeAt(0);
+      if (code >= 0xac00 && code <= 0xd7af) {
+        return acc + 2;
+      }
+      return acc + 1;
+    }, 0);
+  };
+
+  const visualLength = getLength(name);
+
+  const spanClasses =
+    visualLength >= 5 ? styles.alignStart : styles.alignCenter;
+
+  const profileClasses = classnames(
     styles.profile,
     type === ProfileType.NavigationBar ? styles.naviType : "",
     styles[size],
     typographyStyles.xsSemiBold
   );
-  const spanClasses = name.length >= 3 ? styles.alignStart : styles.alignCenter;
 
   return (
-    <div className={NavigationBarClasses} style={{ backgroundColor: color }}>
+    <div className={profileClasses} style={{ backgroundColor: color }}>
       <span className={spanClasses}>{name}</span>
     </div>
   );
