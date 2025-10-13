c13f6a2fd152a4b27832709d5dcfa28068e53dd5
luli <nidor022@gmail.com>
2025-10-13 15:12:34 +0900

    ✨ feat [#50] pr 수정사항 - dashboard 개행

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,6 +3,7 @@ import styles from "./index.module.css";
 import NavigationBar from "@/components/navigationBar/navigation-bar";
 import DashboardSideBar from "@/components/dashboard-side-bar/dashboard-side-bar";
 import Column from "@/components/dashboard/column/column";
+
 export default function DashboardPage() {
   return (
     <div className="DashboardContainer">
