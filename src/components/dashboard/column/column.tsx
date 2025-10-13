153aa92f8364b98fb7617a17791f9fe5358e44f1
Luli Lee🦆 <nidor022@gmail.com>
2025-10-13 11:14:11 +0900

    ✨ feat [#51] column 페이지 개발. css추가 필요

--- src/components/dashboard/column/column.tsx
+++ src/components/dashboard/column/column.tsx
@@ -1,7 +1,34 @@
-export default function Column() {
+import { useEffect, useState } from "react";
+import Card from "@/components/dashboard/card/card";
+import { getCards } from "@/components/dashboard/card/api/dashboard";
+import styles from "./column.module.css";
+import { Card as CardData } from "@/types/card";
+
+interface ColumnProps {
+  columnId: number;
+  onCardClick: (card: CardData) => void;
+}
+
+export default function Column({ columnId, onCardClick }: ColumnProps) {
+  const [cards, setCards] = useState<CardData[]>([]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const { cards } = await getCards({ columnId });
+        setCards(cards);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    loadData();
+  }, [columnId]);
+
   return (
-    <>
-      <h1>대쉬보드 컬럼</h1>
-    </>
+    <section className={styles.columnComtainer}>
+      {cards.map((card, index) => (
+        <Card key={index} card={card} onClick={() => onCardClick(card)} />
+      ))}
+    </section>
   );
 }
