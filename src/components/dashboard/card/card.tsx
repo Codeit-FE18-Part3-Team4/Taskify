a2badaee0160e25ec96e42937fae5686e13e0f5d
Luli Lee🦆 <nidor022@gmail.com>
2025-10-13 11:09:59 +0900

    ✨ feat [#50] card 코드 수정. card별 고정 height값 제거

--- src/components/dashboard/card/card.tsx
+++ src/components/dashboard/card/card.tsx
@@ -1,16 +1,17 @@
 import Profile from "@/components/profile/profile";
 import styles from "./card.module.css";
 import Badge from "@/components/chips/badge/badge";
-import { CardData } from "@/type/card-data";
+import { Card as CardData } from "@/types/card";
 import typographyStyles from "@/components/typography/typography.module.css";
 import Image from "next/image";
 
 interface CardProps {
-  cardData: CardData;
+  card: CardData;
+  onClick?: () => void;
 }
 
-export default function Card({ cardData }: CardProps) {
-  const date = new Date(cardData.dueDate);
+export default function Card({ card, onClick }: CardProps) {
+  const date = new Date(card.dueDate);
   const formattedDate = new Intl.DateTimeFormat("ko-KR", {
     year: "numeric",
     month: "long",
@@ -19,37 +20,42 @@ export default function Card({ cardData }: CardProps) {
   }).format(date);
 
   return (
-    <section
+    <article
       className={`${styles.cardContainer} ${typographyStyles["lg2SemiBold"]}`}
+      onClick={onClick}
+      role="button"
+      tabIndex={0}
+      onKeyDown={(e) => {
+        if (onClick && e.key === "Enter") {
+          e.preventDefault();
+          onClick();
+        }
+      }}
     >
-      {cardData.imageUrl && (
+      {card.imageUrl && (
         <div className={styles.cardImage}>
           <Image
-            src={cardData.imageUrl}
-            alt={cardData.title}
+            src={card.imageUrl}
+            alt={card.title}
+            width={0}
+            height={0}
+            sizes="100vw"
             style={{
-              objectFit: "contain",
-              maxHeight: "178.45px",
               width: "100%",
               height: "auto",
             }}
           />
         </div>
       )}
-      <h3 className={styles.title}>{cardData.title}</h3>
+      <h3 className={styles.title}>{card.title}</h3>
       <div className={styles.badge}>
-        {cardData.tags.length > 0 &&
-          cardData.tags.map((tag, tagIndex) => (
-            <Badge title={tag} key={tag} colorIndex={tagIndex} />
-          ))}
+        {card.tags.length > 0 && card.tags.map((tag) => <Badge title={tag} key={tag} />)}
       </div>
-      <time className={`${typographyStyles["mdMedium"]}`}>{formattedDate}</time>
+      <time className={typographyStyles["mdMedium"]}>{formattedDate}</time>
       <div className={styles.assignee}>
-        <Profile name={cardData.assignee.nickname} />
-        <span className={`${typographyStyles["mdMedium"]}`}>
-          {cardData.assignee.nickname}
-        </span>
+        <Profile name={card.assignee.nickname} />
+        <span className={typographyStyles["mdMedium"]}>{card.assignee.nickname}</span>
       </div>
-    </section>
+    </article>
   );
 }
