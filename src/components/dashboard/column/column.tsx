e5c7ddc08e0f366612951da6d3714968c23a27ad
luli <nidor022@gmail.com>
2025-10-14 05:22:10 +0900

    ✨ feat [#51] dashboard column 개발

--- src/components/dashboard/column/column.tsx
+++ src/components/dashboard/column/column.tsx
@@ -1,15 +1,25 @@
 import { useEffect, useState } from "react";
 import Card from "@/components/dashboard/card/card";
-import { getCards } from "@/components/dashboard/card/api/dashboard";
+import { getCards } from "@/components/dashboard/card/api/cards";
 import styles from "./column.module.css";
 import { Card as CardData } from "@/types/card";
+import SettingSvg from "@/components/navigationBar/setting-svg";
+import Typography from "@/components/typography";
+import PlusSvg from "./plus-svg";
 
 interface ColumnProps {
+  columnTitle: string;
   columnId: number;
   onCardClick: (card: CardData) => void;
+  onClick?: (type: "create" | "modify") => void;
 }
 
-export default function Column({ columnId, onCardClick }: ColumnProps) {
+export default function Column({
+  columnTitle,
+  columnId,
+  onCardClick,
+  onClick,
+}: ColumnProps) {
   const [cards, setCards] = useState<CardData[]>([]);
 
   useEffect(() => {
@@ -25,9 +35,25 @@ export default function Column({ columnId, onCardClick }: ColumnProps) {
   }, [columnId]);
 
   return (
-    <section className={styles.columnComtainer}>
+    <section className={styles.columnContainer}>
+      <div className={styles.columnTitleWrapper}>
+        <div className={styles.columnTitle}>
+          <h3 className={Typography.xlSemiBold}>{columnTitle}</h3>
+          <h3 className={Typography.lgSemiBold}>{cards.length}</h3>
+        </div>
+        <div className={styles.buttonWrapper}>
+          <button onClick={() => onClick?.("create")}>
+            <PlusSvg className={styles.icon} color={"var(--color-brand500"} />
+          </button>
+          <button onClick={() => onClick?.("modify")}>
+            <SettingSvg className={styles.icon} color={"var(--color-gray400"} />
+          </button>
+        </div>
+      </div>
       {cards.map((card, index) => (
-        <Card key={index} card={card} onClick={() => onCardClick(card)} />
+        <div>
+          <Card key={index} card={card} onClick={() => onCardClick(card)} />
+        </div>
       ))}
     </section>
   );
