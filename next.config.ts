fbefc258b4b7d8b48eff26910f6cafcbf388e2bb
Chamsol Kim <kcsol1005@gmail.com>
2025-10-14 17:56:07 +0900

    Revert "✨ feat [#74] Sheet에서 사용하는 image input 컴포넌트 개발"
    This reverts commit 6f01fc6ca28254d70e4ccdec343ed70b3ab2bb6b.

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-        pathname: "/**",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
