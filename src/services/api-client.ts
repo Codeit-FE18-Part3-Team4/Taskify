78e604e64043492b016a9781be4c0278208fd013
JinHyuk Kim <dog33027@gmail.com>
2025-10-14 23:16:07 +0900

    🚚 rename [#69] axios-instance -> api-client 로 이름 변경 (다른 로직 추가를 위해 ex. interceptors)

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
+export const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE,
   headers: {
     "Content-Type": "application/json",
   },
 });
-
-export default axiosInstance;
