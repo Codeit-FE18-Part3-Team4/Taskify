431ab452151e9dee0b7cb28b1b7479d3a841262d
jeyoung <jdngh@naver.com>
2025-10-15 21:56:00 +0900

    ♻️ refactor [#55] 필요없는 코드 제거

--- src/features/my-dashboard/api/get-dashboards.ts
+++ src/features/my-dashboard/api/get-dashboards.ts
@@ -3,7 +3,7 @@ import axiosInstanceTest from "@/services/axios-instance-test";
 export async function getDashboards({ page = 1, size = 10} = {}) {
   try {
     const res = await axiosInstanceTest.get(
-      `/dashboards?navigationMethod=pagination&page=${page}&size=${size}&createByME=true`
+      `/dashboards?navigationMethod=pagination&page=${page}&size=${size}`
     );
     const body = res.data;
     console.log(body)
