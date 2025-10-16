a42582197b3f42199f0bcdd00afa9d12e114bf9e
JinHyuk Kim <dog33027@gmail.com>
2025-10-16 16:53:52 +0900

    ✨ feat [#113] axios 인스턴스에 토큰 주입 인터셉터 설정

--- src/services/axios-instance.ts
+++ src/services/axios-instance.ts
@@ -1,3 +1,4 @@
+import { auth } from "@/features/auth/components/auth-provider";
 import axios from "axios";
 
 const axiosInstance = axios.create({
@@ -6,5 +7,34 @@ const axiosInstance = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+axiosInstance.interceptors.request.use((config) => {
+  console.log("Request Interceptor Triggered , auth token:", auth.token);
+
+  if (!auth.token) {
+    delete config.headers.Authorization;
+    return config;
+  }
+
+  if (config.headers) {
+    config.headers.Authorization = `Bearer ${auth.token}`;
+  }
+
+  return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (
+      error.response?.status === 401 &&
+      typeof window !== "undefined" &&
+      !window.location.pathname.includes("/login")
+    ) {
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
