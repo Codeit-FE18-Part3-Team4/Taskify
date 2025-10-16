b69ddd2388c2296cd666f8b87355a6f39a864724
Chamsol Kim <kcsol1005@gmail.com>
2025-10-16 18:05:40 +0900

    ♻️ refactor [] Type을 모아서 내보내는 index module 추가

---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export type { Card } from "./card";
+export type { Column } from "./column";
+export type { Comment } from "./comment";
+export type { Dashboard } from "./dashboard";
+export type { MemberInfo } from "./member-info";
