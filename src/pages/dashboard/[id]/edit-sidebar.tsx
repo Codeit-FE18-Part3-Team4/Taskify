87cd3dba552e3a6aeadf666ff8a6c720ce2f3c58
luli <nidor022@gmail.com>
2025-10-17 11:01:56 +0900

    ✨feat [#117] dashboard edit 페이지 sidebar, 대시보드 수정페이지 개발

---
+++ src/pages/dashboard/[id]/edit-sidebar.tsx
@@ -0,0 +1,63 @@
+import Typography from "@/components/typography";
+import styles from "./edit.module.css";
+import { classnames } from "@/utils/classnames";
+import TrashIcon from "@/components/icon/trash-icon";
+
+export enum TabType {
+  Edit = "edit",
+  ModifyMembers = "modifyMembers",
+  Delete = "delete",
+}
+
+interface EditSidebarMenuProps {
+  activeTab: TabType;
+  onTabChange: (type: TabType) => void;
+}
+
+export default function EditSidebar({
+  onTabChange,
+  activeTab,
+}: EditSidebarMenuProps) {
+  return (
+    <>
+      <div className={styles.sidebar}>
+        <div className={styles.sidebarMenu}>
+          <button
+            type="button"
+            className={classnames(
+              Typography.lg2Medium,
+              activeTab === TabType.Edit ? styles.activeButton : "",
+            )}
+            onClick={() => onTabChange?.(TabType.Edit)}
+          >
+            대시보드 편집
+          </button>
+          <i className={styles.betweenLine} />
+          <button
+            type="button"
+            className={classnames(
+              Typography.lg2Medium,
+              activeTab === TabType.ModifyMembers ? styles.activeButton : "",
+            )}
+            onClick={() => onTabChange?.(TabType.ModifyMembers)}
+          >
+            멤버 관리
+          </button>
+          <i className={styles.betweenLine} />
+          <button
+            type="button"
+            className={classnames(
+              styles.trashButton,
+              Typography.lg2Medium,
+              activeTab === TabType.Delete ? styles.activeButton : "",
+            )}
+            onClick={() => onTabChange?.(TabType.Delete)}
+          >
+            <span>대시보드 삭제하기</span>
+            <TrashIcon className={styles.icon} />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
