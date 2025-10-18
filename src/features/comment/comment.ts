27f1d7eb487b96380699ee17d7e2cf3c29a30d62
luli <nidor022@gmail.com>
2025-10-18 09:39:20 +0900

    ✨feat [#140] comments api

--- src/features/comment/comment.ts
+++ src/features/comment/comment.ts
@@ -29,3 +29,54 @@ export async function getComments({
     throw e;
   }
 }
+
+interface updateCommentProps {
+  content: string;
+  commentId: number;
+}
+
+export async function putComment({ content, commentId }: updateCommentProps) {
+  try {
+    const res = await axiosInstance.put(`/comments/${commentId}`, {
+      content,
+    });
+    return res.data;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+interface PostCommentProps {
+  content: string;
+  cardId: number;
+  columnId: number;
+  dashboardId: number;
+}
+
+export async function postComment({
+  content,
+  cardId,
+  columnId,
+  dashboardId,
+}: PostCommentProps) {
+  try {
+    const res = await axiosInstance.post(`/comments`, {
+      content,
+      cardId,
+      columnId,
+      dashboardId,
+    });
+    return res.data;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export async function deleteComment({ commentId }: { commentId: number }) {
+  try {
+    const res = await axiosInstance.delete(`/comments${commentId}`);
+    return res.data;
+  } catch (e) {
+    console.error(e);
+  }
+}
