a6da50aa714e35a2c308da75390d0ddfe2da8a11
jeyoung <jdngh@naver.com>
2025-10-19 23:28:48 +0900

    ✨ feat [#55] 모바일 사이드바 버튼 & 사이드바 on, off 작업

--- src/components/navigationBar/navigation-bar.tsx
+++ src/components/navigationBar/navigation-bar.tsx
@@ -1,24 +1,29 @@
+import { SidebarMenu } from "@/assets/images";
+import SettingSvg from "@/components/icon/setting-svg";
+import UserPlusSvg from "@/components/icon/user-plus-svg";
 import Modal from "@/components/modal";
 import Typography from "@/components/typography";
 import { CommonSize } from "@/constants/common/common-size";
 import { useModal } from "@/hooks/use-modal";
+import { useSsrResponsive } from "@/hooks/use-ssr-responsive";
 import { MemberInfo } from "@/types/member-info";
+import Image from "next/image";
+import Link from "next/link";
 import MemberList from "./member-list";
 import styles from "./navigation-bar.module.css";
-import SettingSvg from "@/components/icon/setting-svg";
-import UserPlusSvg from "@/components/icon/user-plus-svg";
-import Link from "next/link";
 
 interface NavigationBarProps {
   size?: CommonSize;
   members?: MemberInfo[];
   dashboardId?: number | null;
+  onMobileSidebarToggle?: () => void;
 }
 
 export default function NavigationBar({
   size = CommonSize.Large,
   members = [],
   dashboardId = null,
+  onMobileSidebarToggle,
 }: NavigationBarProps) {
   const sizeName = CommonSize[size].toLowerCase();
   const navigationBarClasses = `${styles.navigationBar} ${styles[sizeName]}`;
@@ -43,8 +48,15 @@ export default function NavigationBar({
     showMembers = members;
   }
 
+  const { isMobile } = useSsrResponsive();
+
   return (
     <div className={navigationBarClasses}>
+      {isMobile && (
+        <button className={styles.mobileMenu} onClick={onMobileSidebarToggle}>
+          <Image src={SidebarMenu} width={20} height={20} alt="사이드바 메뉴" />
+        </button>
+      )}
       {dashboardId && (
         <>
           {showMembers.length > 0 && (
