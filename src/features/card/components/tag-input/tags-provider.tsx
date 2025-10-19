97d5c0386f91e137d1e6759d326f1e38a5841f5a
Chamsol Kim <kcsol1005@gmail.com>
2025-10-19 13:38:19 +0900

    ✨ feat [#150] Tag history를 local storage에 저장해서 사용

--- src/features/card/components/tag-input/tags-provider.tsx
+++ src/features/card/components/tag-input/tags-provider.tsx
@@ -1,19 +1,19 @@
 import {
   createContext,
-  Dispatch,
   ReactNode,
-  SetStateAction,
+  useContext,
+  useEffect,
   useState,
 } from "react";
 
 interface ContextValue {
   tagsHistory: string[];
-  setTagsHistory: Dispatch<SetStateAction<string[]>>;
+  addTagsHistory: (tag: string) => void;
 }
 
-export const TagsContext = createContext<ContextValue>({
+const TagsContext = createContext<ContextValue>({
   tagsHistory: [],
-  setTagsHistory: () => {},
+  addTagsHistory: () => {},
 });
 
 interface Props {
@@ -23,9 +23,36 @@ interface Props {
 export default function TagsProvider({ children }: Props) {
   const [tagsHistory, setTagsHistory] = useState<string[]>([]);
 
+  const addTagsHistory = (tag: string) => {
+    localStorage.setItem("tagsHistory", JSON.stringify([...tagsHistory, tag]));
+    setTagsHistory((prev) => {
+      if (prev.includes(tag)) return prev;
+      return [...prev, tag];
+    });
+  };
+
+  useEffect(() => {
+    const storedTagsHistory = localStorage.getItem("tagsHistory");
+    if (storedTagsHistory) {
+      setTagsHistory(JSON.parse(storedTagsHistory));
+    }
+  }, []);
+
   return (
-    <TagsContext value={{ tagsHistory, setTagsHistory }}>
+    <TagsContext value={{ tagsHistory, addTagsHistory }}>
       {children}
     </TagsContext>
   );
+}
+
+export function useTagsHistory() {
+  const context = useContext(TagsContext);
+  if (!context) {
+    throw new Error("useTagsHistory must be used within a TagsProvider");
+  }
+
+  return {
+    tagsHistory: context.tagsHistory,
+    addTagsHistory: context.addTagsHistory,
+  };
 }
