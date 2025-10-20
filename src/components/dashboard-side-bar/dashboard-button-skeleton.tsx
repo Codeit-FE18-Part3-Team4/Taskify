e9781d9bb1f6dca4bb2f6847d6b5b29a516ee533
jeyoung <jdngh@naver.com>
2025-10-20 17:03:24 +0900

    ✨ feat [#55] 사이드바 스켈레톤 반응형

--- src/components/dashboard-side-bar/dashboard-button-skeleton.tsx
+++ src/components/dashboard-side-bar/dashboard-button-skeleton.tsx
@@ -1,19 +1,25 @@
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import styles from "./dashboard-side-bar.module.css";
+import { useSsrResponsive } from "@/hooks/use-ssr-responsive";
 
 export default function DashboardButtonSkeleton() {
+  const { isTablet } = useSsrResponsive();
+
+  const leftSize = isTablet ? 20 : 24;
+  const MainWidth = isTablet ? 120 : 190;
+
   return (
     <div className={`${styles.button} ${styles.dashboardButton}`}>
       <Skeleton
-        width={24}
-        height={24}
+        width={leftSize}
+        height={leftSize}
         baseColor="var(--color-black700)"
         highlightColor="var(--color-black600)"
       />
       <Skeleton
-        width={190}
-        height={24}
+        width={MainWidth}
+        height={leftSize}
         baseColor="var(--color-black700)"
         highlightColor="var(--color-black600)"
       />
